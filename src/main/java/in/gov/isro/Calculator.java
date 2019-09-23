7936d964462b0e182149029730a58972ca0d885d
chandinibhowmik <vchand23@yahoo.com>
2019-09-23 12:33:16 +0530

    my_app 1st commit

---
+++ src/main/java/in/gov/isro/Calculator.java
@@ -0,0 +1,21 @@
+package in.gov.isro;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
+public class Calculator {
+
+	public int add (int x, int y)
+	{
+		return x+y;
+	}
+	
+	public int multiply(int x, int y)
+	{
+		return x*y;
+	}
+	
+	public int subtract(int x, int y)
+	{
+		return x -y;
+	}
+}
